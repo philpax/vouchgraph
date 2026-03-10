c254c167ac855edc008926e88a55f1f8875e0f7d
Philpax <me@philpax.me>
2026-03-10 02:12:53 +0100

    feat: much faster settling

--- src/components/DebugControls.tsx
+++ src/components/DebugControls.tsx
@@ -15,10 +15,10 @@ export interface SimParams {
 export const DEFAULT_SIM_PARAMS: SimParams = {
   simulationGravity: 0.6,
   simulationRepulsion: 2,
-  simulationFriction: 0.9,
+  simulationFriction: 0.7,
   simulationLinkSpring: 0.3,
   simulationLinkDistance: 8,
-  simulationDecay: 3000,
+  simulationDecay: 500,
   nodeSizeMin: 8,
   nodeSizeMax: 16,
   nodeSizeScale: 3,
